ced3d82e28c4e20d62e0a4f9510066c8d37c7fbf
blinktwice2 <52613563+blinktwice2@users.noreply.github.com>
2019-09-05 09:21:03 -0400

    Add files via upload

---
+++ hw1.py
@@ -0,0 +1,50 @@
+def greeting():
+    capture = input("What's your name? ");
+    print("Hello %s"%capture );
+
+
+def greeting1() :
+    capture = input ( "name:" );
+    print( "Hello ", capture );
+
+
+def number():
+    number = input("enter any real number ");
+    numberint = int(number);
+    print("Your number is " + number )
+
+
+def average():
+    first = input("enter your first number ");
+    firstint= float(first);
+    second = input("enter your second number ");
+    secondint= float(second);
+    third = input("enter your third number ");
+    thirdint= float(third);
+    average = ((first+second+third)/3);
+    print("the average is ", average)
+    
+
+def graduate():
+    gradyear = input("What's your class year? ");
+    gradyearint = int(gradyear);
+    yearstogo = gradyearint - 2019;
+    print("You will graduate from USNA in", yearstogo, "years" );
+
+
+def sphere( radius ):
+    A = (4*3.14)*(radius**2);
+    V = (4/3)*(3.14)*(radius**3);
+    print ( "Surface area is", A," and volume is", V);
+    
+radius (3);
+def feet( inches ):
+    feet = inches // 12;
+    rem = inches %12 ;
+    print( feet, " feet ", rem, " inches" );
+
+    //Functions
+
+def volume(
+
+
